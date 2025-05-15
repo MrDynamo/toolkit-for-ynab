71f7b0b27435a1190a3d78f83d9f41f22a67a997
MrDynamo <mrdynamo@dynamiclab.org>
2025-05-15 16:50:41 -0500

    ci: skip ci on release and sync

--- .config/releaserc.js
+++ .config/releaserc.js
@@ -81,7 +81,7 @@ const config = {
       '@semantic-release/git',
       {
         assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
